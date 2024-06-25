f0bd774081d465145bc44a7c604d528741e6efd0
CherryWintKyaw <wintkyawcherry@gmail.com>
2024-06-25 12:26:51 +0630

    first commit

---
+++ orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        ProductNumber: '86578',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'TATA',
+        ProductNumber: '79399',
+        paymentStatus: 'Paid',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Mang',
+        ProductNumber: '74666',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Chimmy',
+        ProductNumber: '83490',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Kookie',
+        ProductNumber: '09958',
+        paymentStatus: 'Paid',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Coya',
+        ProductNumber: '22636',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Glassess',
+        ProductNumber: '74385',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Mac1',
+        ProductNumber: '56732',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Fancy Doll House',
+        ProductNumber: '95737',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Apple 19pro',
+        ProductNumber: '67533',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    }
+]
